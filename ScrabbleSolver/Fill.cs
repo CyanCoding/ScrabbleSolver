01be9945ffd8d89885b7b33e9cfca120e499c4f4
Camden Mac Leod <camden.macleod@pm.me>
2021-10-11 22:29:45 -1000

    Change return value

--- ScrabbleSolver/Fill.cs
+++ ScrabbleSolver/Fill.cs
@@ -66,7 +66,8 @@ namespace ScrabbleSolver {
                     }
                 }
             }
-            return possibleSpots;
+
+            return foundPlaces;
         }
 
         private static void FindPosition(string[,] boxesArray,
