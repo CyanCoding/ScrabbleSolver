3bfaa9200290016e7d5cb99bb21076d1961c27b8
Camden Mac Leod <camden.macleod@pm.me>
2021-10-13 16:18:28 -1000

    Fix changing board array issue

--- ScrabbleSolver/Fill.cs
+++ ScrabbleSolver/Fill.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
+using System.Windows.Media.Animation;
 
 namespace ScrabbleSolver {
     /// <summary>
@@ -14,10 +16,13 @@ namespace ScrabbleSolver {
         /// <param name="boxesArray">The board</param>
         /// <returns>Location data for each position</returns>
         public static List<MainWindow.LocationData> FindAllPlaces(string[,] boxesArray) {
-            List<MainWindow.LocationData> foundPlaces = new List<MainWindow.LocationData>();
+            List<MainWindow.LocationData> foundPlaces =
+                new List<MainWindow.LocationData>();
             foundPlaces.Clear();
             
-            var possibleSpots = 0;
+            // For cloning arrays:
+            //string[,] originalBoard = (string[,]) boxesArray.Clone();
+
 
             for (var i = 0; i < boxesArray.GetLength(0); i++) {
                 for (var j = 0; j < boxesArray.GetLength(1); j++) {
@@ -32,7 +37,6 @@ namespace ScrabbleSolver {
                             l.X = j;
                             foundPlaces.Add(l);
 
-                            possibleSpots++;
                             boxesArray[i - 1, j] = "1";
 
                         }
@@ -42,7 +46,6 @@ namespace ScrabbleSolver {
                             l.X = j;
                             foundPlaces.Add(l);
 
-                            possibleSpots++;
                             boxesArray[i + 1, j] = "1";
                         }
 
@@ -51,7 +54,6 @@ namespace ScrabbleSolver {
                             l.X = j - 1;
                             foundPlaces.Add(l);
 
-                            possibleSpots++;
                             boxesArray[i, j - 1] = "1";
                         }
 
@@ -60,9 +62,17 @@ namespace ScrabbleSolver {
                             l.X = j + 1;
                             foundPlaces.Add(l);
 
-                            possibleSpots++;
                             boxesArray[i, j + 1] = "1";
                         }
+                    }
+                }
+            }
+            
+            // Reset the array after we modified it
+            for (int i = 0; i < 14; i++) {
+                for (int j = 0; j < 14; j++) {
+                    if (boxesArray[i, j] == "1") {
+                        boxesArray[i, j] = "";
                     }
                 }
             }
@@ -77,23 +87,27 @@ namespace ScrabbleSolver {
         /// <param name="index">The index of the location data to check</param>
         /// <param name="letters">The letters to fill</param>
         /// <param name="anagrams">The anagrams list for the letters</param>
-        /// <param name="FoundPlaces">Every found location</param>
+        /// <param name="foundPlaces">Every found location</param>
         /// <returns>An array of locations and boards</returns>
         public static List<MainWindow.NewBoardConfig> FillFromPosition(
             string[,] boxesArray,
             int index,
             string letters,
             List<string> anagrams,
-            List<MainWindow.LocationData> FoundPlaces) {
+            List<MainWindow.LocationData> foundPlaces) {
+            
+            // The resulting board configs
             List<MainWindow.NewBoardConfig> results =
                 new List<MainWindow.NewBoardConfig>();
-
-            MainWindow.LocationData l = FoundPlaces[index];
-
-            MainWindow.LocationData[] upLocations =
+            
+            // The location we are testing
+            MainWindow.LocationData testSpot = foundPlaces[index];
+            
+            // Each of the positions we can go up
+            MainWindow.LocationData[] upSpots =
                 new MainWindow.LocationData[letters.Length];
 
-            int currentY = l.Y;
+            int currentY = testSpot.Y;
             // Finds all the possible up locations and fills them into
             // upLocations
             for (int i = 0; i < letters.Length; i++) {
@@ -101,35 +115,50 @@ namespace ScrabbleSolver {
                     break; // TODO: Fix this! Doesn't work if too large a length
                 }
 
-                if (boxesArray[currentY, l.X] != "") {
+                if (boxesArray[currentY, testSpot.X] != "") {
                     // There's a letter in this position! Move up
                     i--;
                     continue;
                 }
 
-                if (boxesArray[currentY, l.X] == "") {
-                    upLocations[i].X = l.X;
-                    upLocations[i].Y = currentY;
+                if (boxesArray[currentY, testSpot.X] == "") {
+                    upSpots[i].X = testSpot.X;
+                    upSpots[i].Y = currentY;
                 }
             }
             
+            // The previous boxesArray before it was modified
+            // TODO: We have an issue where changing boxesArray in another function is causing this to change
+            var defaultBox = boxesArray;
+            
+            // A list of each position and letter used for a certain combination
+            MainWindow.LocationData[] filledLocations =
+                new MainWindow.LocationData[letters.Length];
+            
             // TODO: We need to go from length 1 to max
-            for (int i = 0; i < anagrams.Count; i++) {
+            foreach (var anagram in anagrams) {
                 for (int j = 0; j < letters.Length; j++) {
-                    boxesArray[upLocations[j].Y, upLocations[j].X] =
-                        anagrams[i][j] + "";
+                    boxesArray[upSpots[j].Y, upSpots[j].X] =
+                        anagram[j] + "";
 
                     MainWindow.LocationData newLocation;
-                    newLocation.X = upLocations[j].X;
-                    newLocation.Y = upLocations[j].Y;
-                    newLocation.Letter = anagrams[i][j] + "";
+                    newLocation.X = upSpots[j].X;
+                    newLocation.Y = upSpots[j].Y;
+                    newLocation.Letter = anagram[j] + "";
 
+                    filledLocations[j] = newLocation;
                     // TODO: Save the current board state with letters filled get letter locations and pass to config add to results.
-                    MainWindow.NewBoardConfig config;
-
-                    //results.Add(newLocation);
-                    //results[i].NewLocations[j] = newLocation;
                 }
+                
+                // Add the new board configuration to results
+                MainWindow.NewBoardConfig config;
+                config.NewLocations = filledLocations.ToList();
+                config.Board = boxesArray;
+
+                results.Add(config);
+                
+                // Reset the board
+                boxesArray = defaultBox;
                 
                 // Print out results
                 for (int j = 0; j < 14; j++) {
@@ -139,7 +168,6 @@ namespace ScrabbleSolver {
 
                     Console.WriteLine();
                 }
-                
             }
             
             
